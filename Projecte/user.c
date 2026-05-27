abedd30216da755f526d5ac4fd867aeba7fc7a60
ohoxdd <hfdezdesevilla@icloud.com>
2026-05-27 13:01:21 +0200

    M5,M6,M7 (pendiente correccion)

--- Projecte/user.c
+++ Projecte/user.c
@@ -1,12 +1,14 @@
 #include <libc.h>
 
-#define WAIT_TIME 19999999
+#define WAIT_TIME 29999999
 
 void clean_screen()
 {
     for(int y = 0; y<25 ; y++)
         for(int x = 0; x<80 ; x++)
             write(1, " ", 1);
+
+    gotoxy(0,0);
 }
 
 void wait(int loops)
@@ -14,14 +16,96 @@ void wait(int loops)
     while(loops) loops--;
 }
 
-/*
-int strlen(char *s)
+int latest_ticks = 0;
+
+int getfps()
 {
-    int n = 0;
-    while (s[n]) n++;
-    return n;
+    int current_ticks = gettime();
+    int ret = (latest_ticks - current_ticks)/18;
+    latest_ticks = current_ticks;
+    return ret;
 }
-*/
+
+void decorateScreen()
+{
+    int x, y;
+    
+    clean_screen();
+    
+    /* Configurar colores: cyan (6) para borde, negro (0) de fondo */
+    set_color(6, 0);  /* fg=6 (cyan), bg=0 (black) */
+    
+    /* Línea superior: 80 iguales */
+    gotoxy(0, 0);
+    for(x = 0; x < 80; x++) {
+        write(1, "=", 1);
+    }
+    
+    /* Líneas 1-23 con bordes */
+    for(y = 1; y < 24; y++) {
+        gotoxy(0, y);
+        write(1, "|", 1);
+        
+        /* Dibuja la cara grande en el medio (y=5 a y=12) */
+        if(y == 5) {
+            gotoxy(32, y);
+            set_color(3, 0);  /* Yellow (3), black background */
+            write(1, "_____________", 13);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 6) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "/             \\", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 7) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "|  o       o  |", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 8) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "|     >       |", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 9) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "|   \\_____/   |", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 10) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "|  HAPPY! :D  |", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 11) {
+            gotoxy(31, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "|             |", 15);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        else if(y == 12) {
+            gotoxy(32, y);
+            set_color(3, 0);  /* Yellow */
+            write(1, "\\___________/", 13);
+            set_color(6, 0);  /* Back to cyan */
+        }
+        
+        gotoxy(79, y);
+        write(1, "|", 1);
+    }
+    
+    /* Línea inferior: 80 iguales */
+    gotoxy(0, 24);
+    for(x = 0; x < 80; x++) {
+        write(1, "=", 1);
+    }
+}
 
 int __attribute__ ((__section__(".text.main")))
 main(void)
@@ -359,6 +443,16 @@ main(void)
     }
 
     write(1, "\n=== ALL SHMAT + SHMDT + SHMRM TESTS COMPLETE ===\n", 49);
+	wait(WAIT_TIME);
+	wait(WAIT_TIME);
 
-    while(1){}
+    write(1,"\n",1);
+    clean_screen();
+    decorateScreen();
+    char buffer[256];
+    while(1){
+        gotoxy(0,0);
+        itoa(getfps(),buffer);
+        write(1,buffer,strlen(buffer));
+    }
 }
